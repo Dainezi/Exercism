f736fed49d3043fc24d56811003cb9d0c83f343d
Dainezi <gabriel.dainezi@hbsis.com.br>
2020-09-08 16:00:46 -0300

    Clock

--- teams/padawan-2020-maringa/csharp/clock/Clock.cs
+++ teams/padawan-2020-maringa/csharp/clock/Clock.cs
@@ -1,19 +1,57 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
-public class Clock
+public class Clock : IEquatable<Clock>
 {
+    private int Hora { get; }
+    private int Minuto { get; }
     public Clock(int hours, int minutes)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Hora = (hours + (minutes / 60)) % 24;
+        Minuto = minutes % 60;
+
+        if (Minuto < 0)
+        {
+            Hora--;
+            Minuto = 60 + Minuto;
+        }
+        if (Hora < 0)
+        {
+            Hora = 24 + Hora;
+        }
+
+
     }
+
 
     public Clock Add(int minutesToAdd)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new Clock(Hora, Minuto + minutesToAdd);
+
     }
 
     public Clock Subtract(int minutesToSubtract)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new Clock(Hora, Minuto - minutesToSubtract);
+    }
+
+    public override string ToString()
+    {
+        return $"{Hora:0#}:{Minuto:0#}";
+    }
+
+    public bool Equals([AllowNull] Clock other)
+    {
+        if (Hora == other.Hora && Minuto == other.Minuto)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
+
     }
 }
