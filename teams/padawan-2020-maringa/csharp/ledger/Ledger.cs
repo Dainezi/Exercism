2f23b0ca2517da9092bc4d12d4fbed6c6606df0d
Dainezi <gabriel.dainezi@hbsis.com.br>
2020-09-17 16:56:25 -0300

    Ledger/markdown/treebuild

---
+++ teams/padawan-2020-maringa/csharp/ledger/Ledger.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+public class LedgerEntry
+{
+    public LedgerEntry(DateTime date, string desc, decimal chg)
+    {
+        Date = date;
+        Desc = desc;
+        Chg = chg;
+    }
+
+    public DateTime Date { get; }
+    public string Desc { get; }
+    public decimal Chg { get; }
+}
+
+public static class Ledger
+{
+    private const string LOCALE_US = "en-US";
+    private const string LOCALE_NL = "nl-NL";
+    private const string CURRENCY_USD = "USD";
+    private const string CURRENCY_EUR = "EUR";
+
+    public static LedgerEntry CreateEntry(string date, string desc, int chng) =>
+        new LedgerEntry(DateTime.Parse(date, CultureInfo.InvariantCulture), desc, chng / 100.0m);
+
+    private static CultureInfo CreateCulture(string cur, string loc)
+    {
+        if ((cur != CURRENCY_USD && cur != CURRENCY_EUR) || (loc != LOCALE_NL && loc != LOCALE_US))
+        {
+            throw new ArgumentException("Invalid currency");
+        }
+
+        var culture = new CultureInfo(loc);
+        culture.NumberFormat.CurrencySymbol = cur == CURRENCY_USD ? "$" : "€";
+        culture.NumberFormat.CurrencyNegativePattern = (loc != LOCALE_US) ? 12 : 0;
+        culture.DateTimeFormat.ShortDatePattern = loc == LOCALE_US ? "MM/dd/yyyy" : "dd/MM/yyyy";
+        return culture;
+    }
+
+    private static string PrintHead(string loc) => (loc == LOCALE_US)
+            ? "Date       | Description               | Change       " : (loc == LOCALE_NL)
+            ? "Datum      | Omschrijving              | Verandering  " : throw new ArgumentException("Invalid locale");
+
+    private static string Date(IFormatProvider culture, DateTime date) => date.ToString("d", culture);
+
+    private static string Description(string desc) => 
+        desc.Length > 25 ? $"{desc.Substring(0, 22)}..." : desc;
+
+    private static string Change(IFormatProvider culture, decimal cgh) =>
+        cgh < 0.0m ? cgh.ToString("C", culture) : cgh.ToString("C", culture) + " ";
+
+    private static string PrintEntry(IFormatProvider culture, LedgerEntry entry) =>
+        $"{Date(culture, entry.Date)} | {Description(entry.Desc),-25} | {Change(culture, entry.Chg),13}";
+
+
+    private static IEnumerable<LedgerEntry> sort(LedgerEntry[] entries)
+    {
+        var result = new List<LedgerEntry>();
+        result.AddRange(entries.Where(e => e.Chg < 0).OrderBy(x => $"{x.Date}@{x.Desc}@{x.Chg}"));
+        result.AddRange(entries.Where(e => e.Chg >= 0).OrderBy(x => $"{x.Date}@{x.Desc}@{x.Chg}"));
+
+        return result;
+    }
+
+    public static string Format(string currency, string locale, LedgerEntry[] entries)
+    {
+        var formatted = PrintHead(locale);
+
+        if (entries.Length > 0)
+        {
+            for (var i = 0; i < sort(entries).Count(); i++)
+            {
+                formatted += $"\n{PrintEntry(CreateCulture(currency, locale), sort(entries).Skip(i).First())}";
+            }
+        }
+        return formatted;
+    }
+}
---
+++ teams/padawan-2020-maringa/csharp/markdown/Markdown.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Markdown
+{
+    // Used string interpolation to reduce the size of the string
+    private static string Wrap(string text, string tag) => $"<{tag}>{text}</{tag}>";
+    // Removed "IsTag" Method that was not being used
+    // Used string interpolation and inserted it directly to the "Method" Replace to make the code easier on the eyes
+    private static string Parse(string markdown, string delimiter, string tag) =>
+        Regex.Replace(markdown, $"{delimiter}(.+){delimiter}", $"<{tag}>$1</{tag}>");
+    private static string ParseText(string markdown, bool isSpecialCharacter)
+    {
+        // Inserted the previously existing Methods "Parse_(ParseItalic) and Parse__(ParseStrong) directly into
+        // parsedText, where they were being used.
+        var parsedText = Parse(Parse(markdown, "__", "strong"), "_", "em");
+        // To Ternary Operator
+        return isSpecialCharacter ? parsedText : Wrap(parsedText, "p");
+    }
+    private static string ParseHeader(string markdown, bool list, out bool inListAfter)
+    {
+        // Changed for loop to While, eliminating the use of "Break"
+        var count = 0;
+        while (markdown[count] == '#')
+            count++;
+        if (count == 0)
+        {
+            inListAfter = list;
+            return null;
+        }
+        // Simplified statement and removed double attribution of false in inListAfter
+        var headerHtml = Wrap(markdown.Substring(count + 1), $"h{count}");
+        inListAfter = false;
+        // Finally, simplified return operation using Ternary Operator
+        return list ? $"</ul>{headerHtml}" : headerHtml;
+    }
+    private static string ParseLineItem(string markdown, bool list, out bool inListAfter)
+    {
+        if (markdown.StartsWith("*"))
+        {
+            var innerHtml = Wrap(ParseText(markdown.Substring(2), true), "li");
+            // Simplified statement, attributed inListAfter to true always and used Ternary Operator to define
+            // the return instead of If.
+            inListAfter = true;
+            return list ? innerHtml : "<ul>" + innerHtml;
+        }
+        inListAfter = list;
+        return null;
+    }
+
+    private static string ParseParagraph(string markdown, bool list, out bool inListAfter)
+    {
+        // Simplified statement, attributed inListAfter to false always and used Ternary Operator to define
+        // the return instead of If.
+        inListAfter = false;
+        return list ? "</ul>" + ParseText(markdown, false) : ParseText(markdown, false);
+    }
+    // Changed all the Ifs for the operator "??" and made it so that the command fits in only one line
+    private static string ParseLine(string markdown, bool list, out bool inListAfter) =>
+        ParseHeader(markdown, list, out inListAfter) ??
+        ParseLineItem(markdown, list, out inListAfter) ??
+        ParseParagraph(markdown, list, out inListAfter) ??
+        throw new ArgumentException("Invalid markdown");
+    public static string Parse(string markdown)
+    {
+        var lines = markdown.Split('\n');
+        var result = string.Empty;
+        var list = false;
+        // Changed For to Foreach to increase readability
+        foreach (var t in lines)
+        {
+            var lineResult = ParseLine(t, list, out list);
+            result += lineResult;
+        }
+        // result = lines.Select(t => ParseLine(t, list, out list))
+        //    .Aggregate(result, (current, lineResult) => current + lineResult);
+        return list ? result + "</ul>" : result;
+    }
+}
